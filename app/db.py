08ddea27252150cbbb255ab3b810cb13b6111b4d
chencheng <291552579@qq.com>
2017-10-26 18:20:08 +0800

    refactor generate db data code

--- app/db.py
+++ app/db.py
@@ -12,3 +12,42 @@ get_head_cmd = """
     where table_schema='snp_index' and table_name='{}';
     """
 get_unique_cmd = "select distinct() from {};"
+
+create_expr_cmd = """
+                  create table {}(Id INT PRIMARY KEY AUTO_INCREMENT,
+                                  GENE_ID VARCHAR(20),
+                                  CHR VARCHAR(5),
+                                  POS_START VARCHAR(50),
+                                  POS_END VARCHAR(50),
+                  """
+
+
+create_snp_cmd = """
+                   create table {}(Id INT PRIMARY KEY AUTO_INCREMENT,
+                                   CHR VARCHAR(5),
+                                   POS VARCHAR(50),
+                                   REF VARCHAR(10),
+                                   ALT VARCHAR(10),
+                                   FEATURE VARCHAR(50),
+                                   GENE VARCHAR(100),
+                   """
+
+create_group_cmd = """
+                   create table {}(Id INT PRIMARY KEY AUTO_INCREMENT,
+                                   SAMPLE VARCHAR(20),
+                                   DESCRIPTION VARCHAR(100),
+                   """
+
+snp_table_info = {'cmd': create_snp_cmd,
+                  'fixed_column_num': 6,
+                  'fixed_column_name': ('CHR', 'POS', 'REF', 'ALT', 'FEATURE', 'GENE'),
+                  'add_key_str': ',key chrindex (CHR), key posindex (POS)'}
+
+expr_table_info = {'cmd': create_expr_cmd,
+                   'fixed_column_num': 4,
+                   'fixed_column_name': ('GENE_ID', 'CHR', 'POS_START', 'POS_END'),
+                   'add_key_str': ',key geneindex (GENE_ID)'}
+                   
+relative_table = {'snp_mRNA_table': 'expr_gene_pos'}
+table_info = {'snp': snp_table_info,
+              'expr': expr_table_info}
--- app/file2db.py
+++ app/file2db.py
@@ -2,23 +2,7 @@
 import sys
 import click
 import MySQLdb
-from db import DATABASE, HOSTBNAME, USERNAME, PASSWORD
-
-create_table_cmd = """
-                   create table {}(Id INT PRIMARY KEY AUTO_INCREMENT,
-                                   CHR VARCHAR(5),
-                                   POS VARCHAR(50),
-                                   REF VARCHAR(10),
-                                   ALT VARCHAR(10),
-                                   FEATURE VARCHAR(50),
-                                   GENE VARCHAR(100),
-                   """
-
-create_group_cmd = """
-                   create table {}(Id INT PRIMARY KEY AUTO_INCREMENT,
-                                   SAMPLE VARCHAR(20),
-                                   DESCRIPTION VARCHAR(100),
-                   """
+from db import DATABASE, HOSTBNAME, USERNAME, PASSWORD, table_info
 
 
 def deal_cell(cell):
@@ -33,36 +17,40 @@ def deal_cell(cell):
 
 
 @click.command()
-@click.option('--filename', help='a file for snp index table or group info')
-def table2db(filename, split='\t', add_key=True):
+@click.option('--filename', help='a file for snp index table or expr table')
+@click.option('--typename', help="a type value is 'snp' or 'expr'")
+def table2db(filename, typename, split='\t', add_key=True):
     '''
-    invert snp index file to mysql table
-    default add index to POS and CHR
+    invert file to mysql table
     '''
+    tableName = filename.rsplit('/')[1]
+    tableName = '_'.join([typename, tableName])
+    fixed_column_num = table_info[typename]['fixed_column_num']
+
     try:
         with open(filename, 'r') as info:
             header = info.readline().strip()
             header_list = header.split(split)
+            # invert all '-' to '_'
             header_list = [each.replace('-', '_') for each in header_list]
-            if len(header_list) <= 6:
-                print 'file header not allowed < 6!'
+            if len(header_list) <= fixed_column_num:
+                print '{0} header not allowed < {1}!'.format(filename,
+                                                             fixed_column_num)
                 sys.exit(1)
             else:
-                header_list[:6] = ('CHR', 'POS', 'REF', 'ALT', 'FEATURE', 'GENE')
+                header_list[:fixed_column_num] = table_info[typename]['fixed_column_name']
             if add_key:
-                add_key_str = ',key chrindex (CHR), key posindex (POS), key geneindex (GENE)'
+                add_key_str = table_info[typename]['add_key_str']
             else:
                 add_key_str = ''
             con = MySQLdb.connect(HOSTBNAME, USERNAME, PASSWORD, DATABASE)
-            tableName = filename.rsplit('/')[1]
-            # tableName = '_'.join(['table', tableName
             with con as cur:
                 cmd = 'drop table if exists {}'.format(tableName)
                 cur.execute(cmd)
                 samples = []
-                for i in header_list[6:]:
+                for i in header_list[fixed_column_num:]:
                     samples.append('{} VARCHAR(20)'.format(i))
-                cmd = create_table_cmd.format(tableName) + ','.join(samples) + add_key_str +');'
+                cmd = table_info[typename]['cmd'].format(tableName) + ','.join(samples) + add_key_str +');'
                 # print cmd
                 cur.execute(cmd)
                 row = info.readline().strip()
@@ -79,60 +67,6 @@ def table2db(filename, split='\t', add_key=True):
         print 'file not find!'
     except MySQLdb.Error as e:
         print 'mysql error {}:{}'.format(e.args[0], e.args[1])
-
-
-def group2db(filename, split='\t'):
-    '''
-    invert snp index group info to mysql table
-    '''
-    try:
-        with open(filename, 'r') as info:
-            header = info.readline()
-            header_list = header.split(split)
-            header_list = [each.replace('-', '_') for each in header_list]
-            if len(header_list) <= 2:
-                print 'file header not allowed < 2!'
-                sys.exit(1)
-            else:
-                header_list[:2] = ('SAMPLE', 'DESCRIPTION')
-            con = MySQLdb.connect(HOSTBNAME, USERNAME, PASSWORD, DATABASE)
-            tableName = filename.rsplit('/')[1]
-            tableName = '_'.join(['group', tableName])
-            with con as cur:
-                cmd = 'drop table if exists {}'.format(tableName)
-                cur.execute(cmd)
-                cultivar = []
-                for i in header_list[2:]:
-                    cultivar.append('{} VARCHAR(20)'.format(i))
-                cmd = create_group_cmd.format(tableName) + ','.join(cultivar) + ')'
-                cur.execute(cmd)
-                row = info.readline().strip()
-                header = ','.join(header_list)
-                while(row):
-                    tmp_list = row.split(split)
-                    tmp_list = ['"'+str(k)+'"' for k in tmp_list]
-                    each_line = ','.join(tmp_list)
-                    cmd = "insert into {0}({1}) values({2})".format(tableName, header, each_line)
-                    cur.execute(cmd)
-                    row = info.readline().strip()
-                print '{} had been wrote into mysql!'.format(tableName)
-    except IOError:
-        print 'file not find!'
-    except MySQLdb.Error as e:
-        print 'mysql error {}:{}'.format(e.args[0], e.args[1])
-
-
-@click.command()
-@click.option('--filename', help='a file for snp index table or group info')
-@click.option('--type', help="a string for 'table' or 'group'")
-def data2db(filename, type):
-    if type == 'table':
-        table2db(filename)
-    elif type == 'group':
-        group2db(filename)
-    else:
-        print 'type error!'
-        sys.exit(1)
 
 
 if __name__ == '__main__':
