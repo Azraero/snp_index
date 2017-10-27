0936644ceeba52bf6d6403d1ac8f6b17a88e3e1e
chenjialin <839588325@qq.com>
2017-10-27 12:15:22 +0800

    aliyun

--- consts.py
+++ consts.py
@@ -2,7 +2,7 @@ import os
 
 HOSTBNAME = 'localhost'
 DATABASE = 'snp_index'
-USERNAME = os.environ.get('USERNAME', 'root')
-PASSWORD = os.environ.get('PASSWORD', '050400')
+USERNAME = os.environ.get('USERNAME', 'onmaisiadmin')
+PASSWORD = os.environ.get('PASSWORD', 'onmaisiadmin')
 DB_URI = 'mysql://{}:{}@{}/{}'.format(
     USERNAME, PASSWORD, HOSTBNAME, DATABASE)
