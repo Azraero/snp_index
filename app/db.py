ab27755ad643991141ce69a9c8cd6190f8a49f45
jamebluntcc <291552579@qq.com>
2017-11-02 19:46:53 +0800

    drop file2db file

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ import os
 
 HOSTBNAME = 'localhost'
 DATABASE = 'snp_index'
-USERNAME = os.environ.get('USERNAME', 'root')
+USERNAME = os.environ.get('USERNAME', 'jamebluntcc')
 PASSWORD = os.environ.get('PASSWORD', '050400')
 DB_URI = 'mysql://{}:{}@{}/{}'.format(
     USERNAME, PASSWORD, HOSTBNAME, DATABASE)
