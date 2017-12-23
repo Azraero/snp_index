40fc2229baed49f6ccb28f0230cc6c10ae91532d
jamebluntcc <291552579@qq.com>
2017-12-23 17:05:16 +0800

    add mail settings

--- settings.py
+++ settings.py
@@ -5,6 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'djaildhjsdfhjs'
+    MAIL_SERVER = 'smtp.exmail.qq.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USER')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWD')
+    MAIL_SUBJECT_PREFIX = ['SNP INDEX']
+    MAIL_SENDER = 'Snp Index Admin <0>'.format(MAIL_USERNAME)
 
     @staticmethod
     def init_app(app):
