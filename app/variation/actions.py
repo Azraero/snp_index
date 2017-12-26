a1aa0060a4016702eb1976661a99491c1d22ea97
chencheng <291552579@qq.com>
2017-12-26 10:24:24 +0800

    fix group path

--- app/variation/actions.py
+++ app/variation/actions.py
@@ -7,17 +7,17 @@ from app.db import DB
 
 SNP_SCRIPT_DIR = '/public/script/snp_index_table'
 SCRIPT_FILE = 'mergeSampleAlleSnpEff.py'
-INPUT_TABLE = ''
+INPUT_TABLE = os.path.join(basedir, 'data', 'mRNA.filter.hq.snp.ann.table')
 
 
 def create_group_info(groupA, groupB, filename):
     groupA_name = filename.split('vs')[0]
     groupB_name = filename.split('vs')[1]
-    with open(filename, 'w+') as f:
+    with open(os.path.join(SNP_SCRIPT_DIR, filename), 'w+') as f:
         for sample in groupA:
-            f.write('\t'.join([sample, groupA_name]) + '\n')
+            f.write('\t'.join([sample.replace('_', '-'), groupA_name]) + '\n')
         for sample in groupB:
-            f.write('\t'.join([sample, groupB_name]) + '\n')
+            f.write('\t'.join([sample.replace('_', '-'), groupB_name]) + '\n')
 
 
 @celery.task
@@ -26,14 +26,16 @@ def run_snp_variations(group_info):
     groupA = group_info[group_name[0]]
     groupB = group_info[group_name[1]]
     create_group_info(groupA, groupB, filename='vs'.join(group_name))
+
     cmd = "python {script} -i {input} -o {output} -g {group} -d {depth}".format(
-        script=SCRIPT_FILE,
+        script=os.path.join(SNP_SCRIPT_DIR, SCRIPT_FILE),
         input=INPUT_TABLE,
         output=os.path.join(basedir, 'app', 'static', 'variation_results', 'vs'.join(group_name) + '_table'),
         group=os.path.join(SNP_SCRIPT_DIR, 'vs'.join(group_name)),
         depth='5'
     )
     subprocess.call(cmd, shell=True)
+    return 'done'
     '''
     db = DB()
     results = db.execute("select email from users where username='{0}'".format(user))
