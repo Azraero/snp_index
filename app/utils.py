4060d54106bbaa49b48a249c598091633d33edbb
jamebluntcc <291552579@qq.com>
2018-01-04 15:27:51 +0800

    add humansize bytes

--- app/utils.py
+++ app/utils.py
@@ -348,3 +348,21 @@ def add_direction(row, sp='|'):
     totalDepth = sum([int(cell) for cell in groupA + groupB])
     row.extend([groupADirect, groupBDirect, str(totalDepth)])
     return row
+
+
+def humanize_bytes(bytesize, precision=2):
+    abbrevs = (
+        (1 << 50, 'PB'),
+        (1 << 40, 'TB'),
+        (1 << 30, 'GB'),
+        (1 << 20, 'MB'),
+        (1 << 10, 'KB'),
+        (1, 'bytes')
+    )
+    if bytesize == 1:
+        return '1 bytes'
+    for factor, suffix in abbrevs:
+        if bytesize >= factor:
+            break
+
+    return '%.*f %s' % (precision, bytesize / factor, suffix)
