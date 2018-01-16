1d34499432e37726a67d0dd93c54f56b6a54af58
jamebluntcc <291552579@qq.com>
2018-01-16 16:21:47 +0800

    add init_db command

--- manager.py
+++ manager.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 from flask_script import Manager, Server
 from flask_migrate import MigrateCommand
-
+from app.auth.models import User, Snptable
+from app.exetensions import db
 from app.app import create_app
 
 app = create_app('prod')
@@ -19,6 +20,27 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.command
+def init_db():
+    db.create_all()
+    chencheng = User(username='chencheng',
+                     email='291552579@qq.com',
+                     password='050400',
+                     actvie=True,
+                     is_admin=True)
+    db.session.add(chencheng)
+    db.session.commit()
+
+    tables = [Snptable(tablename='snp_mRNA_snp_ann_table',
+                       tabletype='snp',
+                       owner='chencheng'),
+              Snptable(tablename='expr_gene_tmp_pos',
+                       tabletype='expr',
+                       owner='chencheng')]
+    db.session.add_all(tables)
+    db.session.commit()
 
 
 if __name__ == '__main__':
